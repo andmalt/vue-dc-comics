4e206a51149cd4923c5b78b4b8ebdf984316812a
andmalt <andrea.maltempi@gmail.com>
2021-09-24 16:52:37 +0200

    inserted header

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,11 @@ export default {
 </script>
 
 <style lang="scss">
+@import "./style/variables.scss";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,23 @@
 <template>
   <header>
+      <div id="img-logo">
+          <img src="../assets/img/dc-logo.png" alt="logo DC">
+      </div>
 
+      <nav id="nav-h">
+          <ul>
+              <li><a href="#">Characters</a></li>
+              <li><a href="#">Comics</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">Tv</a></li>
+              <li><a href="#">Games</a></li>
+              <li><a href="#">Collectibles</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">Fans</a></li>
+              <li><a href="#">News</a></li>
+              <li><a href="#">Shops</a></li>
+          </ul>
+      </nav>
   </header>
 </template>
 
@@ -10,6 +27,35 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "../style/variables.scss";
+
+header{
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+}
+#img-logo{
+    width: 60px;
+    & img{
+        width: 100%;
+    }
+}
+#nav-h{
+    text-align: center;
+    & ul{
+        list-style: none;
+        display: flex;
+        & li{
+            padding: 0 1rem 0;
+            & a{
+                text-decoration: none;
+                color: $colorContent;
+            }
+        }
+    }
+}
+
+
 
 </style>
